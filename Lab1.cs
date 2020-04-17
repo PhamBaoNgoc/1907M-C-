d6cd10a3df211e793511d3ede817aa2422992816
PhamBaoNgoc <59905079+PhamBaoNgoc@users.noreply.github.com>
2020-04-17 12:06:25 +0700

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Lab1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Moi nhap so n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int f = 0;
+            int f1 = 1, f2 = 1;
+            while (f1 + f2 < n)
+            {
+                f = f1 + f2;
+                f2 = f1;
+                f1 = f;
+            }
+            Console.WriteLine("Number = " + n + " Fibonacci = " + f);
+        }
+    }
+}
